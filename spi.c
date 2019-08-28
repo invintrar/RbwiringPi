a455683927efec6fd400f79eae543fd98aa2b85c
invintrar <invintrar@gmail.com>
2019-08-27 19:15:57 -0500

    Modificacion spi.c

--- spi.c
+++ spi.c
@@ -18,10 +18,25 @@ int main(){
 
 
 	for(i = 0; i < 30; i++){
-		data_env[0] = bandera;
-		data_env[1] = 0x00;
+		if(bandera == 0x0A || bandera == 0x0B || bandera == 0x10){
+			data_env[0] = bandera;
+			//Llenamos la matriz para enviar 
+			for(i = 1; i < 6; i++){
+				data_env[i] = 0x00;
+			}
+		}else{
+			data_env[0] = bandera;
+			data_env[1] = 0x00;
+		}
+
 		wiringPiSPIDataRW(CHANNEL, data_env, sizeof(data_env));
-		printf("%6.3X  %14.3X\n", bandera, data_env[1]);
+
+		if(bandera == 0x0A || bandera == 0x0B || bandera == 0x10){
+			printf("%6.2X  ",bandera);
+			printf("%10.2X%.2X%.2X%.2X%.2X\n", data_env[1], data_env[2], data_env[3], data_env[4], data_env[5]);
+		}else{
+			printf("%6.2X  %18.10X\n", bandera, data_env[1]);
+		}
 		bandera++;
 	}
 }
